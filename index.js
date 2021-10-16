b9c41e302e4b70bb46c92624840452d84d555f5f
Kiri <kirilynne@gmail.com>
2021-10-16 11:46:02 -0400

    add initial prompts for user input

--- index.js
+++ index.js
@@ -15,60 +15,25 @@ const teamInput = [
 
     {
         type: 'input',
-        message: 'Provide a description of the project:',
-        name: 'description',
-        validate: (value) => { if (value) { return true } else { return 'Must input a description to continue.' } }
+        message: "What is their employee ID number?",
+        name: 'idnumber',
+        validate: (value) => { if (value) { return true } else { return 'Must input an ID to continue.' } }
     },
 
     {
         type: 'input',
-        message: 'What are the installation instructions for this project?',
-        name: 'installation',
-        validate: (value) => { if (value) { return true } else { return 'Must input installation instructions to continue.' } }
-    },
-
-    {
-        type: 'input',
-        message: 'Describe the usage information:',
-        name: 'usage',
-        validate: (value) => { if (value) { return true } else { return 'Must input usage information to continue.' } }
-    },
-
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines for this project?',
-        name: 'contribution',
-        validate: (value) => { if (value) { return true } else { return 'Must input contribution guidelines to continue.' } }
-    },
-
-    {
-        type: 'input',
-        message: 'Describe the test instructions:',
-        name: 'test',
-        validate: (value) => { if (value) { return true } else { return 'Must input test instructions to continue.' } }
-    },
-
-    {
-        type: 'input',
-        message: 'What is your Github username?',
-        name: 'username',
-        validate: (value) => { if (value) { return true } else { return 'Must input a Github username to continue.' } }
-    },
-
-    {
-        type: 'input',
-        message: 'What is your email address?',
+        message: 'What is their email address?',
         name: 'email',
         validate: (value) => { if (value) { return true } else { return 'Must input an email address to continue.' } }
     },
 
     {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your application.',
-        choices: ['MIT License', 'GNU General Public License', 'Eclipse Public License', 'none'],
-        validate: (value) => { if (value) { return true } else { return 'Must choose a value to continue.' } }
+        type: 'input',
+        message: 'What is their office number?',
+        name: 'office',
+        validate: (value) => { if (value) { return true } else { return 'Must input office number to continue.' } }
     },
+
 ];
 
 // TODO: Create a function to write HTML file
