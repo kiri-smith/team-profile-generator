8e40f381dacf832ae09aea7584ff4900663ba72f
Kiri <kirilynne@gmail.com>
2021-10-16 19:12:31 -0400

    add tests for intern subclass

---
+++ tests/Intern.test.js
@@ -0,0 +1,50 @@
+const Employee = require("../Employee");
+const Intern = require("../Intern");
+
+describe("Intern class", () => {
+    describe("getSchool method", () => {
+        it("should return an object containing a 'school' property when called with the 'new' keyword", () => {
+            // Act
+            const obj = new Intern();
+
+            // Assert
+            expect("school" in obj).toEqual(true);
+        });
+
+        it("should set 'school' when created", () => {
+            // Arrange
+            const username = "Nazareth";
+            // Act
+            const obj = new Intern(school);
+            // Assert
+            expect(obj.school).toEqual("Nazareth");
+        });
+
+        it("should default 'username' to ''", () => {
+            // Act
+            const obj = new Intern();
+            // Assert
+            expect(obj.school).toEqual('');
+        });
+    });
+
+
+    //how do I write this one?
+    describe("getRole method", () => {
+        it("should return the object name when called"), () => {
+            // Act
+            const obj = new Intern();
+
+            // Assert
+            expect(role).toEqual(Intern);
+        };
+
+        it("should return 'role' when created", () => {
+            // Act
+            const obj = new Intern();
+            // Assert
+            expect(role).toEqual("Intern");
+        });
+
+    })
+});
