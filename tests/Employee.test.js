1512b13900277103e62677c9c1b4744d1d387395
Kiri <kirilynne@gmail.com>
2021-10-16 12:59:09 -0400

    finish employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -26,4 +26,74 @@ describe("Employee class", () => {
             expect(obj.name).toEqual('');
         });
     });
+
+    describe("getID method", () => {
+        it("should return an object containing an 'ID' property when called with the 'new' keyword", () => {
+            // Act
+            const obj = new Employee();
+
+            // Assert
+            expect("ID" in obj).toEqual(true);
+        });
+
+        it("should set 'ID' when created", () => {
+            // Arrange
+            const id = "123456";
+            // Act
+            const obj = new Employee(id);
+            // Assert
+            expect(obj.id).toEqual("123456");
+        });
+
+        it("should default 'ID' to ''", () => {
+            // Act
+            const obj = new Employee();
+            // Assert
+            expect(obj.id).toEqual('');
+        });
+    });
+
+    describe("getEmail method", () => {
+        it("should return an object containing an 'email' property when called with the 'new' keyword", () => {
+            // Act
+            const obj = new Employee();
+
+            // Assert
+            expect("email" in obj).toEqual(true);
+        });
+
+        it("should set 'email' when created", () => {
+            // Arrange
+            const email = "jtf003@gmail.com";
+            // Act
+            const obj = new Employee(email);
+            // Assert
+            expect(obj.email).toEqual("jtf003@gmail.com");
+        });
+
+        it("should default 'email' to ''", () => {
+            // Act
+            const obj = new Employee();
+            // Assert
+            expect(obj.email).toEqual('');
+        });
+    });
+
+    //how do I write this one?
+    describe("getRole method", () => {
+        it("should return the object name when called"), () => {
+            // Act
+            const obj = new Employee();
+
+            // Assert
+            expect(role).toEqual(Employee);
+        });
+
+    it("should return 'role' when created", () => {
+        // Act
+        const obj = new Employee();
+        // Assert
+        expect(role).toEqual("Employee");
+    });
+
 });
