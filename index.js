eae891d8811029186fce98925d0c851ba3373569
Kiri <kirilynne@gmail.com>
2021-10-17 21:07:16 -0400

    add functions for user input based on role

--- index.js
+++ index.js
@@ -5,51 +5,152 @@ const generateFile = require('./utils/generateFile');
 
 // obtain user input
 
-const newTeam = [];
-
-const teamQuestions = [
+const managerQuestions = [
 
     {
         type: 'input',
-        message: "What is your team member's name?",
+        message: "What is your team manager's name?",
         name: 'name',
         validate: (value) => { if (value) { return true } else { return 'Must input a name to continue.' } }
     },
 
     {
         type: 'input',
-        message: "What is their employee ID number?",
+        message: "What is your team manager's employee ID number?",
         name: 'idnumber',
         validate: (value) => { if (value) { return true } else { return 'Must input an ID to continue.' } }
     },
 
     {
         type: 'input',
-        message: 'What is their email address?',
+        message: "What is your team manager's email address?",
         name: 'email',
         validate: (value) => { if (value) { return true } else { return 'Must input an email address to continue.' } }
     },
 
     {
         type: 'input',
-        message: 'What is their office number?',
+        message: "What is your team manager's office number?",
         name: 'office',
         validate: (value) => { if (value) { return true } else { return 'Must input office number to continue.' } }
     },
 
+    {
+
+        type: 'list',
+        name: 'addEmployee',
+        message: "Select the employee type you would like to add or select 'Finish Building My Team' to exit.",
+        choices: ['Engineer', 'Intern', 'Finish Building My Team'],
+        validate: (value) => { if (value) { return true } else { return 'Must choose a value to continue.' } }
+    },
 ];
 
-// TODO: Create a function to write HTML file
+const engineerQuestions = [
+
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'name',
+        validate: (value) => { if (value) { return true } else { return 'Must input a name to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the engineer's employee ID number?",
+        name: 'idnumber',
+        validate: (value) => { if (value) { return true } else { return 'Must input an ID to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the engineer's email address?",
+        name: 'email',
+        validate: (value) => { if (value) { return true } else { return 'Must input an email address to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the engineer's Github username?",
+        name: 'username',
+        validate: (value) => { if (value) { return true } else { return 'Must input a username to continue.' } }
+    },
+];
+
+const internQuestions = [
+
+    {
+        type: 'input',
+        message: "What is the intern's name?",
+        name: 'name',
+        validate: (value) => { if (value) { return true } else { return 'Must input a name to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the intern's employee ID number?",
+        name: 'idnumber',
+        validate: (value) => { if (value) { return true } else { return 'Must input an ID to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the intern's email address?",
+        name: 'email',
+        validate: (value) => { if (value) { return true } else { return 'Must input an email address to continue.' } }
+    },
+
+    {
+        type: 'input',
+        message: "What is the name of the intern's school?",
+        name: 'school',
+        validate: (value) => { if (value) { return true } else { return 'Must input a school to continue.' } }
+    },
+];
+
+function newManager() {
+    inquirer.prompt(managerQuestions)
+        .then((responses) => {
+            return new Manager();
+        })
+};
+
+function newEngineer() {
+    inquirer
+        .prompt(engineerQuestions)
+        .then((responses) => {
+            return new Engineer();
+        })
+};
+
+function newIntern() {
+    inquirer.prompt(internQuestions)
+        .then((responses) => {
+            return new Intern();
+        })
+};
+
+const newTeam = [];
+
+//ASK ARM:
+//push to empty array
+//repeat the questions for more employees
+//exit when "finish..." selected
+//init function
+
+// Create a function to write HTML file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((responses) => {
+    newTeam.push(newManager /* how do I put them all here? */) => {
         writeToFile('NewIndex.html', generateFile({ ...responses }))
     })
 }
 
-// Function call to initialize app
+//Call it to initialize app
 init();
+
+
+
